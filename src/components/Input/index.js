5905fa1dc10487f8524609db7989e91817ceee90
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-01 02:49:00 -0300

    Componente de input criado

---
+++ src/components/Input/index.js
@@ -0,0 +1,7 @@
+import { Input } from './style'
+
+export default function InputComponent({ inputValue, inputChangeHandler }) {
+    return (
+        <Input type="text" value={inputValue} onChange={(e) => inputChangeHandler(e.target.value)} />
+    )
+}
---
+++ src/components/Input/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+    width: 288px;
+    height: 48px;
+    border-radius: 2px;
+    box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+    background: #fff;
+    border: none;
+    padding: 15px 16px;
+
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    color: var(--gray-500);
+    line-height: 18px;
+    font-size: 16px;
+
+    :focus {
+        outline: none;
+    }
+
+`
